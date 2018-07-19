351a858286b4fe22dc3573cfebc5f8e05082eda3
zhgilevi <38855124+zhgilevi@users.noreply.github.com>
2018-07-19 12:36:49 +0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,37 +1,15 @@
-
-
-
-import os
+from flask import Flask,request,jsonify
 import telebot
-from flask import Flask, request
+token="683907622:AAFDIZxPvAQJXqFQNc4zwGgggmvs9EICr8c"
 
-TOKEN = '683907622:AAFDIZxPvAQJXqFQNc4zwGgggmvs9EICr8c'
-bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+app = Flask(__name__)
+bot=telebot.TeleBot(token)
+bot.set_webhook(url='https://easybot123.herokuapp.com/')
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
-
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://easybot123.herokuapp.com/' + TOKEN)
-    return "!", 200
-
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+@app.route('/',methods=["GET","POST"])
+def hello_world():
+    if request.method=="POST":
+        return request.get_json()
+    bot.send_message(421590404,"Hello from heroku")
+    return ('Hello World!',200,None)
