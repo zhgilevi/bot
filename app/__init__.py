5d78af53f6ce365ed574e5fb49c979af0615c1e7
zhgilevi <38855124+zhgilevi@users.noreply.github.com>
2018-07-19 12:56:59 +0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,12 @@ token="683907622:AAFDIZxPvAQJXqFQNc4zwGgggmvs9EICr8c"
 
 app = Flask(__name__)
 bot=telebot.TeleBot(token)
-global count=0
+
 
 
 @app.route('/',methods=["POST","GET"])
 def hello_world():
-    global count
+     count=0
     if request.method == "POST":
         count+=1
         return (request.get_json(),200,None)
