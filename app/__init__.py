23b42f5c855ce5218fcb2e51ab7d7efef19931df
zhgilevi <38855124+zhgilevi@users.noreply.github.com>
2018-07-19 12:58:23 +0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,10 +9,9 @@ bot=telebot.TeleBot(token)
 
 @app.route('/',methods=["POST","GET"])
 def hello_world():
-     count=0
     if request.method == "POST":
         count+=1
         return (request.get_json(),200,None)
     else:
-        bot.send_message(421590404,"Hello from heroku")
-        return (count,200,None)
+ 
+        return ('Hi',200,None)
