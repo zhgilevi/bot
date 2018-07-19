c9392ec47e36de19664898882e8a5a24f862d53f
zhgilevi <38855124+zhgilevi@users.noreply.github.com>
2018-07-19 12:39:36 +0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,7 @@ bot=telebot.TeleBot(token)
 bot.set_webhook(url='https://easybot123.herokuapp.com/')
 
 
-@app.route('/',methods=["GET","POST"])
+@app.route('/')
 def hello_world():
-    if request.method=="POST":
-        return request.get_json()
     bot.send_message(421590404,"Hello from heroku")
     return ('Hello World!',200,None)
