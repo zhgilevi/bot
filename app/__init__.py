6830d234cdd19d1e241dc50839ff8d90d6900b24
ilich <ilich@example.com>
2018-07-19 11:55:14 +0500

    just

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import telebot
+token="683907622:AAFDIZxPvAQJXqFQNc4zwGgggmvs9EICr8c"
+
+app = Flask(__name__)
+bot=telebot.TeleBot(token)
+
+@app.route('/')
+def hello_world():
+    bot.send_message(421590404,"Hello from heroku")
+    return ('Hello World!',200,None)
